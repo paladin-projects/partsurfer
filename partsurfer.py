48eba332bddaa59a1386624f133b149acfcaec3e
Ivan Lyanguzov <tzong@ya.ru>
2022-09-11 10:50:56 +0300

    Switch to HPE Partsurfer Mobile5

--- partsurfer.py
+++ partsurfer.py
@@ -1,31 +1,46 @@
 import argparse
 from urllib.request import urlopen
+from urllib.parse import urlencode
+from html.parser import HTMLParser
 import xml.etree.ElementTree as ET
 
 parser = argparse.ArgumentParser(description='Fetch spare parts details fro HPE Partsurfer based on serial, product or part number')
-parser.add_argument('-s', '--serial', help='search for serial number')
+parser.add_argument('-s', '--serial', nargs='+', help='search for serial number')
 args = parser.parse_args()
 
-flag = 0
-text = []
-boundaries = {'serial': '<table cellspacing="0" cellpadding="2" rules="all" border="1" id="ctl00_BodyContentPlaceHolder_gridSpareBOM" style="background-color:White;font-size:12px;width:660px;border-collapse:collapse;table-layout:fixed">',
-              'product': '<table class="rtf_table">'}
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global count
+        if tag == 'div' and count > 0:
+            count = count + 1
+        if tag == 'div' and ('id', 'tab2') in attrs:
+            count = 1
 
-with urlopen('https://partsurfer.hpe.com/Search.aspx?searchText=' + args.serial) as response:
-    for line in response:
-        line = line.decode().strip()
-        if not len(line):
-            continue
-        if line == boundaries['serial']:
-            flag = 1
-        if line == '</table>' and flag:
-            text.append(line)
-            flag = 0
-            break
-        if flag:
-            text.append(line.replace('&nbsp;', ' '))
+    def handle_endtag(self, tag):
+        global count
+        if tag == 'div' and count > 0:
+            count = count - 1
 
-for row in ET.fromstringlist(text).iter('tr'):
-    for span in row.iter('span'):
-        print(span.text, end='\t')
-    print()
+parser = MyHTMLParser()
+
+for serial in args.serial:
+    count = 0
+    text = []
+    with urlopen('https://partsurfermobile.ext.hpe.com/', data=urlencode({'SelectedCountryID': '', 'SearchString': serial}).encode('ascii')) as response:
+    #with urlopen('file:index.html.1') as response:
+        for line in response:
+            line = line.decode().strip()
+            if not len(line):
+                continue
+            parser.feed(str(line))
+            if count > 0:
+                text.append(line.replace('&','&amp;'))
+        text.append('</div>')
+    for row in ET.fromstringlist(text).iter('ul'):
+        a = row.find('li[1]')
+        b = a.find('a')
+        if b != None:
+            print(b.text, end='\t')
+        b = a.find('strong')
+        if b != None:
+            print(b.tail)
