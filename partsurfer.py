b5f5294d08fb6e402df554163c2e54ea7ec0eac1
Ivan Lyanguzov <tzong@ya.ru>
2023-09-05 14:11:48 +0300

    Add product search, first attepmt

--- partsurfer.py
+++ partsurfer.py
@@ -1,55 +1,53 @@
-import argparse
 import sys
+import argparse
 from urllib.request import urlopen
 from urllib.parse import urlencode
-from html.parser import HTMLParser
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+import csv
 
-parser = argparse.ArgumentParser(description='Fetch spare parts details fro HPE Partsurfer based on serial, product or part number', argument_default=argparse.SUPPRESS)
+parser = argparse.ArgumentParser(description='Fetch spare parts details fro HPE Partsurfer based on serial, product or part number')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-s', '--serial', action='store_true', help='search for serial number(s)')
-group.add_argument('-p', '--product', action='store_false', help='search for product number(s)')
-group.add_argument('-n', '--part', action='store_false', help='search for part number(s)')
+group.add_argument('-p', '--product', action='store_true', help='search for product number(s)')
+group.add_argument('-n', '--part', action='store_true', help='search for part number(s)')
 parser.add_argument('NUM', nargs='+', help='number(s) to search for')
+parser.add_argument('-o', '--output', help='send output to file')
 args = parser.parse_args()
 
+# print(args)
 if len(sys.argv) == 1:
     parser.print_usage()
-    parser.exit()
+    parser.exit(1)
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        global count
-        if tag == 'div' and count > 0:
-            count = count + 1
-        if tag == 'div' and ('id', 'tab2') in attrs:
-            count = 1
+if args.output:
+    f = open(args.output, 'w', newline='')
+else:
+    f = sys.stdout
 
-    def handle_endtag(self, tag):
-        global count
-        if tag == 'div' and count > 0:
-            count = count - 1
+csv_writer = csv.writer(f)
 
-parser = MyHTMLParser()
-
-for serial in args.NUM:
-    count = 0
-    text = []
-    with urlopen('https://partsurfermobile.ext.hpe.com/', data=urlencode({'SelectedCountryID': '', 'SearchString': serial}).encode('ascii')) as response:
-    #with urlopen('file:index.html.1') as response:
-        for line in response:
-            line = line.decode().strip()
-            if not len(line):
-                continue
-            parser.feed(str(line))
-            if count > 0:
-                text.append(line.replace('&','&amp;'))
-        text.append('</div>')
-    for row in ET.fromstringlist(text).iter('ul'):
-        a = row.find('li[1]')
-        b = a.find('a')
-        if b != None:
-            print(b.text, end='\t')
-        b = a.find('strong')
-        if b != None:
-            print(b.tail)
+for num in args.NUM:
+    with urlopen('https://partsurfermobile.ext.hpe.com/', data=urlencode({'SelectedCountryID': '', 'SearchString': num}).encode('ascii')) as response:
+        page = BeautifulSoup(response.read(), 'lxml')
+        if page.find('div', class_='message error'):
+            print('Error for {}'.format(num), file=sys.stderr)
+        if args.serial:
+            csv_writer.writerow(['Serial','Part','Description'])
+        if args.product:
+            csv_writer.writerow(['Product','Part','Description'])
+            items = page.find_all('ul' , class_='cols2 compare')
+            # print(lines)
+            # sys.exit()
+            r = [num]
+            for i in items:
+                lines = i.find_all('strong')
+                for l in lines:
+                    if l.text == 'Part No: ':
+                        r.append(l.next_sibling)
+                    if l.text == 'Description: ':
+                        r.append(l.next_sibling)
+                        csv_writer.writerow(r)
+                        r = [num]
+                        break
+        if args.part:
+            csv_writer.writerow(['Part','Description'])
